f3228f14864d5d231cd88852b74ebb3bc7d083fe
Willy Huallpa <willy.huallpa@pucp.edu.pe>
2024-04-15 23:16:53 -0500

    primera version de bottom bar

--- app/src/main/java/com/example/aurora/MainActivity.java
+++ app/src/main/java/com/example/aurora/MainActivity.java
@@ -1,24 +1,61 @@
 package com.example.aurora;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.aurora.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+            int menuItemId =  item.getItemId();
+
+            if (menuItemId ==  R.id.home) {
+                replaceFragment(new HomeFragment());
+                return true;
+            }
+            else if (menuItemId ==  R.id.create) {
+                replaceFragment(new CreateFragment());
+                return true;
+            }
+            else if (menuItemId ==  R.id.configuration) {
+                replaceFragment(new ConfigurationFragment());
+                return true;
+            }
+            return true;
         });
+
+    }
+    private  void replaceFragment (Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.commit();
+
     }
 }

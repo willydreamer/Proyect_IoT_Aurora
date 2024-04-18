394c1c940c5cfc43e0c396a5b0a6e7b961cd0ab4
Willy Huallpa <willy.huallpa@pucp.edu.pe>
2024-04-18 14:34:36 -0500

    guion asesino

--- app/src/main/java/com/example/aurora/Administrador.java
+++ app/src/main/java/com/example/aurora/Administrador.java
@@ -7,18 +7,55 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.aurora.databinding.ActivityMainBinding;
 
 public class Administrador extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_administrador);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+            int menuItemId =  item.getItemId();
+
+            if (menuItemId ==  R.id.home) {
+                replaceFragment(new HomeFragment());
+                return true;
+            }
+            else if (menuItemId ==  R.id.create) {
+                replaceFragment(new CreateFragment());
+                return true;
+            }
+            else if (menuItemId ==  R.id.supervisor) {
+                replaceFragment(new AdminSupervisoresFragment());
+                return true;
+            }
+            else if (menuItemId ==  R.id.configuration) {
+                replaceFragment(new ConfigurationFragment());
+                return true;
+            }
+            else if (menuItemId ==  R.id.sites) {
+                replaceFragment(new AdminSitiosFragment());
+                return true;
+            }
+            return true;
         });
+
+    }
+    private  void replaceFragment (Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.commit();
+
     }
 }

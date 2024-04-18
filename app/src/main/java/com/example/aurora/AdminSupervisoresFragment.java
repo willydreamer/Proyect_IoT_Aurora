ed808971abc0c6bd6f8ecb2cd5898f17c49f408f
Lenovo <calledchristian79@gmail.com>
2024-04-18 17:50:09 -0500

    commit 5 admin

--- app/src/main/java/com/example/aurora/AdminSupervisoresFragment.java
+++ app/src/main/java/com/example/aurora/AdminSupervisoresFragment.java
@@ -1,11 +1,16 @@
 package com.example.aurora;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class AdminSupervisoresFragment extends Fragment {
     @Override
@@ -14,4 +19,42 @@ public class AdminSupervisoresFragment extends Fragment {
 
         return inflater.inflate(R.layout.admin_fragment_supervisor, container, false);
     }
+
+
+
+    //para transladar de una vista a otra
+    /*@Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Configura el botón o acción que iniciará la transición al otro fragmento
+        view.findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Obtiene el FragmentManager
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+
+                // Inicia una transacción de fragmento
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                // Reemplaza el fragmento actual con el nuevo fragmento
+                Fragment nuevoFragmento = new InformacionSupervisorActivity();
+                fragmentTransaction.replace(R.id.infosupers, nuevoFragmento);
+
+                // Puedes agregar el fragmento actual a la pila de retroceso
+                // fragmentTransaction.addToBackStack(null);
+
+                // Realiza la transacción
+                fragmentTransaction.commit();
+            }
+        });
+    }
+    /*public void irInfoSupervisor(View view) {
+
+        //primero crear el intento
+        Intent intent = new Intent(AdminSupervisoresFragment.this, InformacionSupervisorActivity.class);
+        //iniciar activity
+        Log.d("Iot", "si");
+        startActivity(intent);
+    }*/
 }
--- app/src/main/java/com/example/aurora/InformacionSupervisorActivity.java
+++ app/src/main/java/com/example/aurora/InformacionSupervisorActivity.java
@@ -1,12 +1,17 @@
 package com.example.aurora;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
 public class InformacionSupervisorActivity extends AppCompatActivity {
 
@@ -16,6 +21,15 @@ public class InformacionSupervisorActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_informacion_supervisor);
     }
+
+    /* Como Fragment
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Infla el layout de tu fragmento
+        return inflater.inflate(R.layout.activity_informacion_supervisor, container, false);
+    }*/
+
 
 
 }

0ae34dc4869b57fda2cdf84e40be25871621fcba
Lenovo <calledchristian79@gmail.com>
2024-04-17 21:01:00 -0500

    commit 4 admin

--- app/src/main/java/com/example/aurora/ListaSupervisoresActivity.java
+++ app/src/main/java/com/example/aurora/ListaSupervisoresActivity.java
@@ -1,8 +1,16 @@
 package com.example.aurora;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ListaSupervisoresActivity extends AppCompatActivity {
@@ -13,5 +21,34 @@ public class ListaSupervisoresActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_lista_supervisores);
 
-        }
+        /*Button btnVer = findViewById(R.id.button1);
+
+        btnVer.setOnClickListener(view -> {
+            Intent intent = new Intent(ListaSupervisoresActivity.this, InformacionSupervisorActivity.class);
+            launcher.launch(intent);
+        });*/
     }
+
+    /*ActivityResultLauncher<Intent> launcher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult o) {
+
+                }
+            }
+    );*/
+
+
+
+    public void irInfoSupervisor(View view) {
+
+        //primero crear el intento
+        Intent intent = new Intent(this, InformacionSupervisorActivity.class);
+        //iniciar activity
+        Log.d("Iot", "si");
+        startActivity(intent);
+
+    }
+
+}

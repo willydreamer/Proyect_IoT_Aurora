054fedfaabf4ff3d3ac25da1e2d5972da05dd68a
Alejandro Gutierrez Chavarria <113729680+adawa111@users.noreply.github.com>
2024-05-31 23:21:46 -0500

    superadmin change xml

--- app/src/main/java/com/example/aurora/SuperAdmin.java
+++ app/src/main/java/com/example/aurora/SuperAdmin.java
@@ -2,6 +2,8 @@ package com.example.aurora;
 
 import android.os.Bundle;
 
+import com.example.aurora.ui.dashboard.DashboardFragment;
+import com.example.aurora.ui.notifications.NotificationsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,15 +28,30 @@ public class SuperAdmin extends AppCompatActivity {
         binding = ActivitySuperAdminBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_super_admin);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+            int menuItemId =  item.getItemId();
+
+            if (menuItemId ==  R.id.navigation_home) {
+                replaceFragment(new HomeFragment());
+                return true;
+            }
+            else if (menuItemId ==  R.id.agregar) {
+                replaceFragment(new NotificationsFragment());
+                return true;
+            }
+            else if (menuItemId ==  R.id.navigation_dashboard) {
+                replaceFragment(new DashboardFragment());
+                return true;
+            }
+            else if (menuItemId ==  R.id.navigation_notifications) {
+                replaceFragment(new NotificationsFragment());
+                return true;
+            }
+            return true;
+        });
+
     }
 
     private  void replaceFragment (Fragment fragment){

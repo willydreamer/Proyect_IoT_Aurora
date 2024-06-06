910360a92e9ee957c817793d1ca74d60b0b87c52
milenelarosa <a20206456@pucp.edu.pe>
2024-06-05 20:35:15 -0500

    Cambios que hagan que se caiga el proyecto probablemente

--- app/src/main/java/com/example/aurora/SupervisorAgregarFragment.java
+++ app/src/main/java/com/example/aurora/SupervisorAgregarFragment.java
@@ -1,12 +1,18 @@
 package com.example.aurora;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,33 +21,14 @@ import android.view.ViewGroup;
  */
 public class SupervisorAgregarFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    TextView textView36;
     public SupervisorAgregarFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SupervisorAgregarFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static SupervisorAgregarFragment newInstance(String param1, String param2) {
         SupervisorAgregarFragment fragment = new SupervisorAgregarFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,10 +36,9 @@ public class SupervisorAgregarFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        new IntentIntegrator(this).initiateScan();
+        textView36 = getView().findViewById(R.id.textView36);
+
     }
 
     @Override
@@ -60,5 +46,15 @@ public class SupervisorAgregarFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_supervisor_agregar, container, false);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+
+        String idEquipo = result.getContents();
+        textView36.setText(idEquipo);
     }
 }
